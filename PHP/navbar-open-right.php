8ed99e89d065055e8ac0c807e1f5fd95d71ae892
Vikas Jangid <04viky@gmail.com>
2021-08-28 21:01:11 +0530

    navbar done and banner1 is incomplete

---
+++ PHP/navbar-open-right.php
@@ -0,0 +1,45 @@
+<nav class="header navbar bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/LINKUS SOLUTION/index.php">
+                        <img src="./Images/home_icon/Linkus_Logo@72x.png" alt="LOGO" width="30" height="24" class="d-inline-block align-text-top">
+                        <span class="linkus-logo-text h2 text-white">LINKUS SOLUTIONS</span>
+                        <div>
+                            <span class="linkus-logo-text-bottom text-white h3 float-right">Fostering Horizons</span>
+                        </div>
+                    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav ml-auto menu-li justify-content-end flex-grow-1 pe-3">
+         <li class="nav-item active">
+                                <a class="nav-link text-white" href="./index.php">HOME <span class="sr-only"></span></a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link text-white" href="./about.php">ABOUT US <span class="sr-only"></span></a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-white" href="" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    SERVICES
+                                </a>
+                                <ul class="dropdown-menu menu-li border-white" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="text-white dropdown-item" href="./services.php">Action</a></li>
+                                    <li><a class="text-white dropdown-item" href="./services.php">Another action</a></li>
+                                    <li><a class="text-white dropdown-item" href="./services.php">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link text-white" href="./contact.php">CONTACT US <span class="sr-only"></span></a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link text-white" href="/inchtomilez_files/login.php"><i class="fa fa-user"></i><span class="sr-only"></span> </a>
+                            </li>
+      </div>
+    </div>
+  </div>
+</nav>
