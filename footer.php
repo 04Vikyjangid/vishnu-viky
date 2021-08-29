34ae371104bcac20c32ab98141fa9b8c72308c3b
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-08-29 19:56:30 +0530

    footer completed

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="container-fluid text-center h5">
+    Developed by
+    <a title="Aditya Jain Developer Indore Official Website" href="https://www.aadityajain.dev/" ref="dofollow"
+        target="_blank" class="text-white text-decoration-none fw-bold">Aditya Jain</a>
+    &amp;
+    <span class="fw-bold"> Vikas Jangid</span>
+</div>
+</body>
+<script src="./js/bootstrap.bundle.min.js"></script>
+
+</html>
