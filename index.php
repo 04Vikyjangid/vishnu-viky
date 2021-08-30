2dddfcecf1386e3c0caee0ed93505db140d03499
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-08-31 00:37:07 +0530

    our-expertise updated according to new UI

--- index.php
+++ index.php
@@ -30,18 +30,16 @@ include('header.php');
 
 <section id="our-expertise" class="bg-black">
     <div class="container">
-        <p class="text-center h1 my-5 ff-philosopher fs-5rem">
+        <p class="text-center h1 my-5 ff-azonix fs-5rem">
             Our Expertise
         </p>
 
-        <p class="text-center h4 mb-4 px-5 d-none d-md-block ff-raleway">
-            Whether you are in need of full-cycle RPO, contingent workforce management, creative digital solutions or
-            a blend of all - LinkUs have you covered
+        <p class="text-center h5 mb-4 px-5 d-none d-md-block ff-raleway text-capitalize">
+            Looking for full fledged RPO, Digital & IT Solutions ? LinkUs Have You Covered
         </p>
 
-        <p class="text-center h4 mb-4 d-sm-block d-md-none ff-raleway">
-            Whether you are in need of full-cycle RPO, contingent workforce management, creative digital solutions or
-            a blend of all - LinkUs have you covered
+        <p class="text-center h5 mb-4 d-sm-block d-md-none ff-raleway text-capitalize">
+            Looking for full fledged RPO, Digital & IT Solutions ? LinkUs Have You Covered
         </p>
 
 
@@ -49,82 +47,52 @@ include('header.php');
             <br />
         </div>
 
-        <div class="row mt-5">
+        <div class="row">
 
-            <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-5 text-center mt-5">
                 <img src="./media/images/home-expertise-img1.png" alt="">
-                <p class="h3 ff-philosopher home-expertise-title">
+                <p class="h3 ff-raleway home-expertise-title mt-3">
                     Recrutment Process Outsourcing
                 </p>
-                <p class="mt-3">
-                    As RPO LinkUs Solution provides a company's HR or Resource function and placing people into the
-                    clients organization. We deliver staff, technology, and methodology as per client's requirement.
-                </p>
             </div>
 
 
-            <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-5 text-center mt-5">
                 <img src="./media/images/home-expertise-img2.png" alt="">
-                <p class="h3 ff-philosopher home-expertise-title">
+                <p class="h3 ff-raleway home-expertise-title mt-3">
                     Digital Marketing Solutions
                 </p>
-                <p class="mt-3">
-                    As IT solutions linkUs Solution provides a set of related software programs and/or services that are
-                    sold as a single package. LinkUs Solutions will provide several different services that helps
-                    customers with the guidance and support needed to maintain efficient and effective IT systems.
-                </p>
             </div>
 
 
-            <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-5 text-center mt-5">
                 <img src="./media/images/home-expertise-img3.png" alt="">
-                <p class="h3 ff-philosopher home-expertise-title">
+                <p class="h3 ff-raleway home-expertise-title mt-3">
                     Payrol Handeling & Management
                 </p>
-                <p class="mt-3">
-                    If you are looking for payroll management process, LinkUs Solution is here to administrate a
-                    company, employee, financial records. This would include details of the employee's salaries,
-                    incentives, bonuses, deductions, and net pay.
-                </p>
             </div>
 
-            <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-5 text-center mt-5">
                 <img src="./media/images/home-expertise-img4.png" alt="">
-                <p class="h3 ff-philosopher home-expertise-title">
+                <p class="h3 ff-raleway home-expertise-title mt-3">
                     Training & Development
                 </p>
-                <p class="mt-3">
-                    Looking for enhancing your knowledge, skills and competencies? Look no further than LinkUs
-                    Solutions. In today, world full of competition, we at LinkUs aim at nourishing the talent of not
-                    only individuals, but also the organisations. With best practice knowledge, our training experts
-                    help our customers achieve workforce performance.
-                </p>
             </div>
 
 
-            <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-5 text-center mt-5">
                 <img src="./media/images/home-expertise-img5.png" alt="">
-                <p class="h3 ff-philosopher home-expertise-title">
+                <p class="h3 ff-raleway home-expertise-title mt-3">
                     Counselling
                 </p>
-                <p class="mt-3">
-                    Facing job dissatisfaction? Confused about the right career path? Then all you need is a counselling
-                    session from great professionals. From helping choose the course, college to choosing the right job,
-                    LinkUs Solutions here for youl
-                </p>
             </div>
 
 
 
-            <div class="col-sm-12 col-md-6 col-lg-4 mb-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 mb-5 text-center mt-5">
                 <img src="./media/images/home-expertise-img6.png" alt="">
-                <p class="h3 ff-philosopher home-expertise-title">
+                <p class="h3 ff-raleway home-expertise-title mt-3">
                     Corporate Events
-                </p>
-                <p class="mt-3">
-                    Stressing over planning and organising an event? We are here to take away your stress! LinkUs
-                    Solutions is your one-stop event solutions provider that offers a wide range of corporate event
-                    management and creative services.
                 </p>
             </div>
 
@@ -134,6 +102,8 @@ include('header.php');
 
 
         </div>
+        <div class="text-center"> <button type="button"
+                class="btn btn-outline-light text-uppercase btn-lg rounded-pill fw-bold">Get a quote</button></div>
     </div>
 </section>
 
