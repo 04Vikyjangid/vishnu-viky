729cede787a7cdb36eb9e78bf0b86d7024f25b2e
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-09-05 04:59:07 +0530

    about page section 3 completed

--- about_us.php
+++ about_us.php
@@ -85,6 +85,63 @@ include('header.php');
 </section>
 
 
+<section id="about-why-us" class="mt-4">
+    <div class="container-fluid px-2 px-md-5">
+        <div class="row">
+
+            <div class="col-12 col-md-6 text-center my-4">
+                <img src="./media/images/about-img2.png" alt="about-img2" class="about-img2">
+            </div>
+
+
+            <div class="col-12 col-md-6">
+                <h3 class="h1 ff-azonix">Why Us?</h3>
+                <p class="h5 w-75 ff-raleway d-none d-lg-block">
+                    We are a team of passionate who work in all horizons
+                    serving your human & business needs anywhere
+                    globally. We strive hard to keep our commitment by
+                    delivering best in class services & data driven - high
+                    end digital products to our clients. With our mindful
+                    approach, innovative strategies, and quality-centric
+                    delivery, we have been able to stand apart in the
+                    market setting benchmark of quality assurance.
+                    Joining hands with us will reward you with best
+                    team delivering possibilities adding support to boost
+                    your ROI that too, at a reasonable cost.
+
+                </p>
+                <p class="h5 w-100 ff-raleway d-block d-lg-none">
+                    We are a team of passionate who work in all horizons
+                    serving your human & business needs anywhere
+                    globally. We strive hard to keep our commitment by
+                    delivering best in class services & data driven - high
+                    end digital products to our clients. With our mindful
+                    approach, innovative strategies, and quality-centric
+                    delivery, we have been able to stand apart in the
+                    market setting benchmark of quality assurance.
+                    Joining hands with us will reward you with best
+                    team delivering possibilities adding support to boost
+                    your ROI that too, at a reasonable cost.
+
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+    <div class="d-none d-md-block">
+        <div class="d-flex justify-content-between align-items-center my-3">
+            <img src="./media/images/home-grey-triangle-left.png" alt="home-grey-triangle-left"
+                class="left-grey-triangle">
+            <img src="./media/images/home-digitalPatner-line.png" alt="home-digitalPatner-line" class="home-line">
+        </div>
+    </div>
+</section>
+
 
 <?php
 include('footer.php');
