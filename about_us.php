f29b2f15e8c6d80317f9746315c6590f716feae5
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-09-05 04:27:12 +0530

    About page section 1 completed

--- about_us.php
+++ about_us.php
@@ -3,7 +3,31 @@ $pageTitle = 'About Us';
 include('header.php');
 ?>
 
+<section>
+    <div class="container text-center mt-5">
+        <div class="my-5 d-none d-md-block"></div>
 
+
+        <img src="./media/images/about-hello.png" alt="hello" class="mt-5 w-50">
+        <p class="h3 ff-raleway d-none d-md-block">
+            LinkUs is a multipurpose solutions based company established in HEART of INDIA, delivering its widest range
+            of
+            services across ASIA.
+        </p>
+        <p class="h5 ff-raleway d-block d-md-none">
+            LinkUs is a multipurpose solutions based company established in HEART of INDIA, delivering its widest range
+            of
+            services across ASIA.
+        </p>
+    </div>
+
+    <div class="d-none d-md-block">
+        <div class="d-flex justify-content-between align-items-center my-3">
+            <img src="./media/images/home-digitalPatner-line.png" alt="home-digitalPatner-line" class="home-line">
+            <img src="./media/images/home-pink-triangle-right.png" alt="home-pink-triangle-left" class="home-triangle">
+        </div>
+    </div>
+</section>
 
 
 
