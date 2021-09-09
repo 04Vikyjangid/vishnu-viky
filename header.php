2c45f26efd3daa8b446d8b6b23f5f57d576622d2
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-09-10 03:52:10 +0530

    conditionally adding page specific CSS

--- header.php
+++ header.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./CSS/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="./CSS/style.css" />
-    <link rel="stylesheet" type="text/css" href="./CSS/timeline.css" />
-    <link rel="stylesheet" type="text/css" href="./CSS/login.css" />
+    <?php if($pageTitle == 'Login & Sign up'){echo '<link rel="stylesheet" type="text/css" href="./CSS/login.css" />';} ?>
+    <?php if($pageTitle == 'Home'){echo '<link rel="stylesheet" type="text/css" href="./CSS/timeline.css" />\n';} ?>
     <link rel="stylesheet" type="text/css" href="./CSS/index.css" />
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
 </head>
 
 
